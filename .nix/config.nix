5c8bdf70873635e55ddbdbf4961df0ddad1f195d
Cyril Cohen <cyril.cohen@inria.fr>
2025-07-01 18:13:54 +0200

    forcing univ poly + using appropriate PR

--- .nix/config.nix
+++ .nix/config.nix
@@ -1,7 +1,8 @@
 {
   format = "1.0.0";
   attribute = "hierarchy-builder";
-  default-bundle = "rocq-9.1";
+  no-rocq-yet = true;
+  default-bundle = "coq-universes-clauses";
   bundles = let
     mcHBcommon = {
       mathcomp.override.version = "master";
@@ -55,6 +56,24 @@
       coquelicot.job = false;
     }; };
 
+    "rocq-universes-and-variances" = {
+      rocqPackages = {
+        rocq-core.override.version = "mattam82:universes-and-variances";
+        stdlib.override.version = "mattam82:universes-and-variances";
+        rocq-elpi.override.version = "mattam82:universes-and-variances";
+        rocq-elpi.override.elpi-version = "2.0.7";
+        bignums.override.version = "master";
+      };
+      coqPackages = mcHBcommon // {
+        coq.override.version = "mattam82:universes-and-variances";
+        stdlib.override.version = "mattam82:universes-and-variances";
+        coq-elpi.override.version = "mattam82:universes-and-variances";
+        coq-elpi.override.elpi-version = "2.0.7";
+        bignums.override.version = "master";
+        coquelicot.job = false;
+      };
+    };
+
     "rocq-9.2" = { rocqPackages = mcHBcommon // {
       rocq-core.override.version = "9.2";
       micromega-plugin.override.version = "master";
@@ -75,8 +94,8 @@
       coq.override.version = "9.0";
     }; };
   };
-  cachix.coq = {};
-  cachix.coq-community = {};
+  cachix.coq = { };
+  cachix.coq-community = { };
   cachix.math-comp.authToken = "CACHIX_AUTH_TOKEN";
 
 }
